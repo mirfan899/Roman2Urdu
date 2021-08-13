82d54f8f7cd4b98d9fcf05bc370fb53a76f9b239
Muhammad Irfan <virtuoso.irfan@gmail.com>
2021-08-13 21:15:51 +0500

    fixed None response for ijunoon.

--- helpers.py
+++ helpers.py
@@ -5,12 +5,19 @@ import requests
 def get_transliteration(paragraph=None):
     url = "https://www.ijunoon.com/transliteration/urdu-to-roman/"
     post_data = {"text": paragraph}
-
-    r = requests.post(url, data=post_data)
-    return r
+    try:
+        r = requests.post(url, data=post_data)
+        return r
+    except:
+        print("Issue with response from https://www.ijunoon.com/transliteration/urdu-to-roman/")
+        return False
 
 
 def ijunoon(r):
-    soup = BeautifulSoup(r.text, 'html.parser')
-    transliterate = soup.find("div", {"id": "ctl00_inpageResult"}).text
-    return transliterate
+    if r:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        transliterate = soup.find("div", {"id": "ctl00_inpageResult"}).text
+
+        return transliterate
+    else:
+        return None
--- scrape.py
+++ scrape.py
@@ -3,8 +3,9 @@ from helpers import ijunoon, get_transliteration
 lines = open("data/makhzan_urdu.txt", "r").readlines()
 roman = open("data/makhzan_roman.txt", "w")
 
-for line in lines:
+for index, line in enumerate(lines):
     trans = ijunoon(get_transliteration(line.strip()))
-    roman.write(trans.strip() + "\n")
+    if trans:
+        roman.write(str(index) + " " + trans.strip() + "\n")
 
 roman.close()
