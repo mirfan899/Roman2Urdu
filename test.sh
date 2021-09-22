a072005976925ca3a84485887ad9a43c15e4aed2
mirfan899 <virtuoso.irfan@gmail.com>
2021-09-22 17:21:01 +0800

    data and shell scripts added.

---
+++ test.sh
@@ -0,0 +1,3 @@
+DECODER=/home/plp/marian/build/marian-decoder
+$DECODER -m model.npz -v data/vocab.ro.yml data/vocab.ur.yml --devices 0 < input.txt
+echo "mukammal press release ka matan darj zail hai" | $DECODER -c model.npz.decoder.yml > output.txt
---
+++ train.sh
@@ -0,0 +1,7 @@
+MARIAN=/home/plp/marian/build/marian
+$MARIAN \
+    --train-sets data/makhzan_roman_cleaned.txt data/makhzan_urdu_cleaned.txt \
+	  --type transformer \
+    --vocabs data/vocab.ro.yml data/vocab.ur.yml \
+    --model model.npz
+
