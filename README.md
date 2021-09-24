750a1ac8828ef0c7c9fa3d37dcc0b1318f81d10b
Muhammad Irfan <virtuoso.irfan@gmail.com>
2021-09-24 18:10:44 +0500

    minor changes.

--- README.md
+++ README.md
@@ -43,7 +43,7 @@ Now starts training.
 MARIAN=/home/plp/marian/build/marian
 $MARIAN \
     --train-sets data/makhzan_roman_cleaned.txt data/makhzan_urdu_cleaned.txt \
-	--type transformer \
+    --type transformer \
     --vocabs data/vocab.ro.yml data/vocab.ur.yml \
     --model model.npz
 ```
--- vocab_fix.py
+++ vocab_fix.py
@@ -1,14 +1,14 @@
 import yaml
 
 # roman fix
-# with open("data/vocab.ro.yml", encoding="utf8") as f:
-#     rdata = yaml.load(f, Loader=yaml.SafeLoader)
-#
-# for i, key in enumerate(rdata):
-#     rdata[key] = i
-#
-# with open('vocab.clean.ro.yml', 'w', encoding="utf8") as f:
-#     yaml.dump(rdata, f)
+with open("data/vocab.ro.yml", encoding="utf8") as f:
+    rdata = yaml.load(f, Loader=yaml.SafeLoader)
+
+for i, key in enumerate(rdata):
+    rdata[key] = i
+
+with open("vocab.clean.ro.yml", "w", encoding="utf8") as f:
+    yaml.dump(rdata, f)
 
 # Urdu fix
 with open("data/vocab.ur.yml", encoding="utf8") as f:
@@ -17,5 +17,5 @@ with open("data/vocab.ur.yml", encoding="utf8") as f:
 for i, key in enumerate(udata):
     udata[key] = i
 
-with open('vocab.clean.ur.yml', 'w') as f:
+with open("vocab.clean.ur.yml", "w") as f:
     yaml.dump(udata, f, allow_unicode=True)
